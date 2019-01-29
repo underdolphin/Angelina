7d2bc670a3533f8ff04b67d1ade53509a8203751
underdolphin <underdolphin@users.noreply.github.com>
2019-01-29 22:21:56 +0900

    Update Mainview

--- Angelina.Settings/View/MainWindow.xaml.cs
+++ Angelina.Settings/View/MainWindow.xaml.cs
@@ -1,18 +1,4 @@
 ﻿using MahApps.Metro.Controls;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Angelina.Settings.View
 {
@@ -24,12 +10,13 @@ namespace Angelina.Settings.View
         public MainWindow()
         {
             InitializeComponent();
+            HamburgerMenu.SelectedIndex = 0;
         }
 
         private void HamburgerMenu_ItemClick(object sender, ItemClickEventArgs e)
         {
-            this.HamburgerMenu.Content = e.ClickedItem;
-            this.HamburgerMenu.IsPaneOpen = false;
+            HamburgerMenu.Content = e.ClickedItem;
+            HamburgerMenu.IsPaneOpen = false;
         }
 
         private void HamburgerMenu_OptionsItemClick(object sender, ItemClickEventArgs e)
